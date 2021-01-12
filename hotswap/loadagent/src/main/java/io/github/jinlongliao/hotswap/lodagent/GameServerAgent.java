1e3122da4b7e7a06644aed8a090b5399ebeec73c
liaojinlong <jinlongliao@foxmail.com>
2021-01-12 12:14:59 +0800

    # Java 热更新

---
+++ hotswap/loadagent/src/main/java/io/github/jinlongliao/hotswap/lodagent/GameServerAgent.java
@@ -0,0 +1,101 @@
+package io.github.jinlongliao.hotswap.lodagent;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.instrument.ClassDefinition;
+import java.lang.instrument.Instrumentation;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 动态代理加载器
+ *
+ * @author liaojinlong
+ * @since 2021/1/7 20:14
+ */
+public class GameServerAgent {
+
+
+    public static final String SPLIT = ";";
+
+    public static void agentmain(String args, Instrumentation inst) throws Exception {
+        System.out.println("agent 启动成功,开发重定义对象....");
+        if (args != null && args.trim().length() > 0) {
+            String[] classFileNames = args.split(SPLIT);
+            if (classFileNames.length > 1) {
+                // ROOT 目录
+                String rootPath = classFileNames[0];
+                Map<String, File> fileMap = new HashMap<>(classFileNames.length);
+                for (int i = 1; i < classFileNames.length; i++) {
+                    String classFileName = classFileNames[i];
+                    File file = new File(classFileName);
+                    fileMap.put(getClassName(file, rootPath), file);
+                }
+
+                Class<?>[] allClass = inst.getAllLoadedClasses();
+                for (Class<?> aClass : allClass) {
+                    String className = aClass.getName();
+                    if (fileMap.containsKey(className)) {
+                        try {
+                            byte[] bytes = fileToBytes(fileMap.get(className));
+                            System.out.println(String.format("文件大小：%s Class：%s", bytes.length, className));
+                            ClassDefinition classDefinition = new ClassDefinition(aClass, bytes);
+                            inst.redefineClasses(classDefinition);
+                        } catch (IOException e) {
+                            System.out.println("热更新失败...." + e.getLocalizedMessage());
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println("热更新成功....");
+    }
+
+    /**
+     * 获取 ClassName
+     *
+     * @param file
+     * @param rootPath
+     * @return ClassName
+     */
+    private static String getClassName(File file, String rootPath) {
+        String filePath = file.getPath();
+        filePath = filePath.replace(getPath(rootPath), "");
+        String className = filePath.split("\\.")[0];
+        filePath = className.replaceAll(getSplit(File.separator), ".");
+        return filePath;
+    }
+
+    private static String getPath(String path) {
+        if (path.endsWith(File.separator)) {
+            return path;
+        }
+        return path + File.separator;
+    }
+
+    /**
+     * 文件转 字节数组
+     *
+     * @param file
+     * @return /
+     * @throws IOException
+     */
+    private static byte[] fileToBytes(File file) throws IOException {
+        try (FileInputStream in = new FileInputStream(file)) {
+            byte[] bytes = new byte[in.available()];
+            in.read(bytes);
+            in.close();
+            return bytes;
+        } catch (IOException e) {
+            throw e;
+        }
+    }
+
+    private static String getSplit(String separator) {
+        if (separator.equals("\\")) {
+            return "\\\\";
+        }
+        return separator;
+    }
+}
---
+++ hotswap/swaptrigger/src/main/java/io/github/jinlongliao/hotswap/trigger/HotSwapHttpServletContext.java
@@ -0,0 +1,144 @@
+package io.github.jinlongliao.hotswap.trigger;
+
+
+import com.sun.tools.attach.VirtualMachine;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import java.io.File;
+import java.io.FileFilter;
+import java.lang.management.ManagementFactory;
+import java.util.*;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author liaojinlong
+ * @since 2021/1/7 19:06
+ */
+public class HotSwapHttpServletContext implements ServletContextListener {
+    private static final Logger log = LoggerFactory.getLogger(HotSwapHttpServletContext.class);
+    private static String PATCHER_DIR;
+    private static String AGENT_PATH;
+    private static ScheduledThreadPoolExecutor poolExecutor = new ScheduledThreadPoolExecutor(2);
+    private boolean init = false;
+    private static final FileFilter FILTER = new FileFilter() {
+        @Override
+        public boolean accept(File pathname) {
+            return pathname.isDirectory() || pathname.getPath().endsWith(".class");
+        }
+    };
+    private static Map<String, Long> fileStatusCache = new HashMap<>(18);
+    private static String PID;
+    private static final String SPILT = ";";
+    private String period;
+    private String interval;
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        String name = ManagementFactory.getRuntimeMXBean().getName();
+        //这里为了方便测试，打印出来进程id
+        PID = name.split("@")[0];
+        String realPath = sce.getServletContext().getRealPath("/");
+        String agentName = sce.getServletContext().getInitParameter("AGENT_NAME");
+        this.period = sce.getServletContext().getInitParameter("PERIOD");
+        this.interval = sce.getServletContext().getInitParameter("INTERVAL");
+        if (period == null || period.trim().length() == 0) {
+            this.period = "5";
+        }
+        if (this.interval == null || this.interval.trim().length() == 0) {
+            this.interval = "5";
+        }
+        if (realPath.endsWith(File.separator)) {
+            PATCHER_DIR = realPath + "patch";
+        } else {
+            PATCHER_DIR = realPath + File.separator + "patch";
+        }
+        AGENT_PATH = PATCHER_DIR + File.separator + agentName;
+        startSchedule();
+    }
+
+    /**
+     * 定时替换
+     */
+    private void startSchedule() {
+        poolExecutor.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                log.info("JavaClass热更新 ");
+                File patch = new File(PATCHER_DIR);
+                if (patch.exists()) {
+                    List<File> listFiles = getFiles(patch);
+                    if (!init) {
+                        for (File item : listFiles) {
+                            fileStatusCache.put(item.getPath(), item.lastModified());
+                        }
+                        init = true;
+                    } else {
+                        Map<String, Long> temp = new HashMap<>(18);
+                        for (File file : listFiles) {
+                            String path = file.getPath();
+                            long modified = file.lastModified();
+                            boolean swap = (!fileStatusCache.containsKey(path)) || modified != fileStatusCache.get(path);
+                            if (swap) {
+                                temp.put(path, modified);
+                            }
+                        }
+                        fileStatusCache.putAll(temp);
+                        if (temp.size() > 0) {
+                            try {
+                                StringBuffer keys = new StringBuffer(PATCHER_DIR);
+                                keys.append(SPILT);
+                                for (String key : fileStatusCache.keySet()) {
+                                    keys.append(key);
+                                    keys.append(SPILT);
+                                }
+                                String substring = keys.substring(0, keys.length() - 1);
+                                //VirtualMachine是jdk中tool.jar里面的东西，所以要在pom.xml引用这个jar
+                                VirtualMachine vm = VirtualMachine.attach(PID);
+                                log.info("Agent 链接成功");
+
+                                // 这个路径是相对于被热更的服务的，也就是这个pid的服务，也可以使用绝对路径。
+                                vm.loadAgent(AGENT_PATH, substring);
+                                vm.detach();
+                            } catch (Exception e) {
+                                log.error("JavaClass热更新  失败", e);
+                            }
+                        } else {
+                            log.info("不存在可用更新");
+                        }
+                    }
+
+
+                } else {
+                    log.info("补丁文件夹不存在");
+                }
+            }
+        }, Long.parseLong(interval), Long.parseLong(period), TimeUnit.MINUTES);
+    }
+
+    /**
+     * 获取Files
+     *
+     * @param patch
+     * @return /
+     */
+    private List<File> getFiles(File patch) {
+        List<File> files = new ArrayList<>(18);
+        for (File file : patch.listFiles(FILTER)) {
+            if (file.isDirectory()) {
+                files.addAll(getFiles(file));
+            } else {
+                files.add(file);
+            }
+        }
+        return files;
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        poolExecutor.shutdown();
+    }
+}
