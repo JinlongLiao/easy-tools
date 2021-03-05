ef1d1fc4d0a148d3b0205b4c22b54a689a81a889
liaojinlong <jinlongliao@foxmail.com>
2021-03-05 14:49:31 +0800

    - 更新代码注释

--- commons/commons-mapstruct/src/main/java/io/github/jinlongliao/commons/mapstruct/core/InnerCoreDataConverter.java
+++ commons/commons-mapstruct/src/main/java/io/github/jinlongliao/commons/mapstruct/core/InnerCoreDataConverter.java
@@ -1,6 +1,20 @@
+/**
+ * Copyright 2020-2021 JinlongLiao
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.github.jinlongliao.commons.mapstruct.core;
 
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -12,6 +26,14 @@ import java.util.Map;
  * @since 2020/11/23 21:14
  */
 public class InnerCoreDataConverter {
+    /**
+     * @param data
+     * @return Object
+     */
+    public static Object getObject(Object data) {
+        return data;
+    }
+
     /**
      * 数据转换为byte
      *
